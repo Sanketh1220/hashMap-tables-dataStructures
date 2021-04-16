9e0b85262202b145f8672a70b8e104e7befe7918
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-16 15:56:00 +0530

    ADD: linkedHashMap and paranoid test case

--- src/test/java/com/hashtablesandtree/HashMapTest.java
+++ src/test/java/com/hashtablesandtree/HashMapTest.java
@@ -22,4 +22,23 @@ public class HashMapTest {
             Assertions.assertEquals(2, frequency);
         }
     }
+
+    @Test
+    public void givenASentence_whenWordsAreAddedToList_ShouldReturnParanoidFrequency(){
+        String sentence = "Paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
+
+        HashMap<String , Integer > linkedHashMap = new HashMap<>();
+        String[] sentenceToWords = sentence.toLowerCase().split(" ");
+        for (String word : sentenceToWords) {
+            Integer value = linkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value = value + 1;
+            linkedHashMap.add(word,value);
+        }
+        int frequency = linkedHashMap.get("paranoid");
+        Assertions.assertEquals(3,frequency);
+    }
 }
